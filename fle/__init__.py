31ec594d1511a071c2bf8f171006ad21dc9c591a
Harvus00 <77236201+Harvus00@users.noreply.github.com>
2025-09-09 19:44:48 -0400

    Update Syntaxt on commd
    Clean up

--- fle/__init__.py
+++ fle/__init__.py
@@ -3,4 +3,4 @@
 # Make submodules available
 from fle import agents, env, eval, cluster, commons
 
-__all__ = ["agents", "env", "eval", "cluster", "commons"]
+__all__ = ["agents", "env", "eval", "cluster", "commons"]   
